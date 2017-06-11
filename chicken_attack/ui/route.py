8e3b88139c6b89609c8fb37d4a388a59cc7fd3fd
Terry <terry@example.com>
2017-06-11 21:08:26 +0800

    move php web to flask

--- chicken_attack/ui/route.py
+++ chicken_attack/ui/route.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from flask import render_template
 
+class WebInfo:
+    Title = '欣興桃園廠生產資料系統'
+
 UI_ROUTE = Blueprint(
     'ui_route',
     __name__,
@@ -8,9 +11,25 @@ UI_ROUTE = Blueprint(
     static_folder='static')
 
 @UI_ROUTE.route('/')
+@UI_ROUTE.route('/monitor')
 def root_html():
-    return render_template('index.html')
+    return render_template('monitor.html',WebInfo=WebInfo)
 
-@UI_ROUTE.route('/page_1')
-def page_1_html():
-    return render_template('page_1/index.html')
+
+@UI_ROUTE.route('/history')
+def history_html():
+    return render_template('history.html',WebInfo=WebInfo)
+
+@UI_ROUTE.route('/templatelist')
+def templatelist_html():
+    return render_template('templatelist.html',WebInfo=WebInfo)
+
+@UI_ROUTE.route('/edittemplate')
+@UI_ROUTE.route('/edittemplate/<templateID>')
+def edittemplate_html(templateID=''):
+    return render_template('edittemplate.html',WebInfo=WebInfo,templateID=templateID)
+
+@UI_ROUTE.route('/editmachine')
+@UI_ROUTE.route('/editmachine/<machineID>')
+def editmachine_html(machineID=''):
+    return render_template('editmachine.html',WebInfo=WebInfo,machineID=machineID)
---
+++ chicken_attack/ui/static/js/AppConfig.js
@@ -0,0 +1,61 @@
+﻿var Debug=true;
+var DataSoureceUrl = "http://192.168.11.130:9487/v1";
+var martiniApiDataSoureceUrl = "http://125.227.156.104/martini/0.2/api";
+var QonfigApiDataSoureceUrl = "http://125.227.156.104/v2";
+var WebServiceUrl = "http://192.168.11.130";
+if(Debug)
+{
+	DataSoureceUrl = "http://localhost:5000";
+	martiniApiDataSoureceUrl = "http://localhost:5000";
+	QonfigApiDataSoureceUrl = "http://localhost:5000";
+	WebServiceUrl = "http://localhost:5000";
+}
+var martiniPagingSize=20;
+var martiniApiDevicesURL = martiniApiDataSoureceUrl + "/devices";
+var martiniApiDeviceChannelsURL = martiniApiDataSoureceUrl + "/devices/{deviceId}/channels";
+var martiniApiDeviceChannelDataURL = martiniApiDataSoureceUrl + "/devices/{deviceId}/channels/{channelId}/data";
+var martiniApiDeviceChannelsLastestDataURL = martiniApiDataSoureceUrl + "/devices/{deviceId}/channels/latest-data";
+var ReadingEquipmentLatestURL = DataSoureceUrl + "/reading/equipments/{equipmentId}/channels/{channelId}/latest";
+var GetAllTemplateURL = DataSoureceUrl + "/config/melsec/templates";
+var GetTemplateURL = DataSoureceUrl + "/config/melsec/templates/{templateId}";
+var GetEquipmentsURL = DataSoureceUrl + "/config/melsec/equipments";
+var ProtocolTypeArray = ["fins", "melsec", "modbus"];
+
+
+
+var GetLightConditionURL = WebServiceUrl + "/GetLightConditions";
+var UpdateLightConditionURL = WebServiceUrl + "/UpdateLightCondition";
+var GetAccountListURL = WebServiceUrl + "/getaccountlist";
+var AddAccountURL = WebServiceUrl + "/addaccount";
+var RemoveAccountURL = WebServiceUrl + "/RemoveAccount";
+var CreateTemplateURL = WebServiceUrl + "/CreateTemplate";
+var DeleteTemplateURL = WebServiceUrl + "/DeleteTemplate/{templateId}";
+var UpdateTemplateURL = WebServiceUrl + "/UpdateTemplate/{templateId}";
+var PutEquipmentsURL = WebServiceUrl + "/UpdateEquipments";
+var EquipmentConfiguration = WebServiceUrl + "/config/{protocoltype}/equipments";
+var EquipmentTemplateList = WebServiceUrl + "/config/{protocoltype}/templates";
+var EquipmentTemplate = WebServiceUrl + "/config/{protocoltype}/templates/{templateId}";
+
+//chart setting 
+var ChartLength=30;
+
+
+//only for testing
+if(Debug)
+{
+	martiniApiDevicesURL = martiniApiDataSoureceUrl + "/static/testing_data/martini-devices.txt";
+	martiniApiDeviceChannelsURL=martiniApiDataSoureceUrl + "/static/testing_data/martini-device-{deviceId}-channels.txt";
+	martiniApiDeviceChannelDataURL=martiniApiDataSoureceUrl + "/static/testing_data/martini-device-{deviceId}-channels-{channelId}-data.txt";
+	martiniApiDeviceChannelsLastestDataURL = martiniApiDataSoureceUrl + "/static/testing_data/devices-{deviceId}-channels-latest-data.txt";
+	ReadingEquipmentLatestURL = DataSoureceUrl + "/test_ReadingEquipmentLatest_{equipmentId}_{channelId}.txt";
+	GetTemplateURL = DataSoureceUrl + "/test_GetTemplate_{templateId}.txt";
+	GetAllTemplateURL = DataSoureceUrl + "/test_GetAllTemplate.txt";
+	GetEquipmentsURL = DataSoureceUrl + "/test_GetEquipments.txt";
+	EquipmentConfiguration = WebServiceUrl + "/static/testing_data/config-{protocoltype}-equipments.txt";
+	EquipmentTemplateList = WebServiceUrl + "/static/testing_data/config-{protocoltype}-templates.txt";
+	EquipmentTemplate = WebServiceUrl + "/static/testing_data/config-{protocoltype}-templates-{templateId}.txt";
+	
+}
+
+
+
