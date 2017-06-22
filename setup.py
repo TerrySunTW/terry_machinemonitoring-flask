79c7988870c997a85198d208d577d963e5a84c40
Terry <terry@example.com>
2017-06-22 18:19:33 +0800

    add number checking

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chicken-attack',
-    version='0.2.0',
+    version='0.2.1',
     description='For terry test front end',
     author='WeiHao Huang',
     author_email='neilhuang@iii.org.tw',
