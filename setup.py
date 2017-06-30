9cdd399bb4ff890e0be0c4569ed6a261dd1a24a2
Terry <terry@example.com>
2017-06-30 16:27:19 +0800

    only send request for avaliable device

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chicken-attack',
-    version='0.2.5',
+    version='0.2.6',
     description='For terry test front end',
     author='WeiHao Huang',
     author_email='neilhuang@iii.org.tw',
