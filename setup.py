88a407e64213810e4e1f801c32c4ce3dc33a373f
Terry <terry@example.com>
2017-08-01 15:23:08 +0800

    add 機台管理

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chicken-attack',
-    version='0.3.12',
+    version='0.3.13',
     description='For terry test front end',
     author='WeiHao Huang',
     author_email='neilhuang@iii.org.tw',
