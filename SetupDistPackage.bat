dbf0bafc7ed2a3eda3362134b9bec8205652a7b2
Terry <terry@example.com>
2017-10-28 23:23:30 +0800

    Adjust batch file

--- SetupDistPackage.bat
+++ SetupDistPackage.bat
@@ -1,2 +1,5 @@
-powershell -Command "(gc C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js) -replace 'var Debug=true;', 'var Debug=false;' | Out-File C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js"
+powershell -Command "(gc C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js) -replace 'var Debug=true;', 'var Debug=false;' | Out-File -Encoding ASCII  C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js"
+
+powershell -Command "(gc c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html) -replace 'type: \"GET\",', 'type: \"POST\",' | Out-File -Encoding ASCII  c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html"
+
 setup.py sdist
--- StartWebService.bat
+++ StartWebService.bat
@@ -1,3 +1,7 @@
-powershell -Command "(gc C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js) -replace 'var Debug=false;', 'var Debug=true;' | Out-File C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js"
+powershell -Command "(gc C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js) -replace 'var Debug=false;', 'var Debug=true;' | Out-File -Encoding ASCII   C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js"
+
+powershell -Command "(gc c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html) -replace 'type: \"POST\",', 'type: \"GET\",' | Out-File -Encoding ASCII  c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html"
+
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:5000/
 python chicken_attack\app.py
+
