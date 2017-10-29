90c53b2cdeada2f274efca350bc76566a109c5ae
Terry <terry@example.com>
2017-10-29 12:13:27 +0800

    adjust batch file for UTF8

--- SetupDistPackage.bat
+++ SetupDistPackage.bat
@@ -1,5 +1,5 @@
-powershell -Command "(gc C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js) -replace 'var Debug=true;', 'var Debug=false;' | Out-File -Encoding ASCII  C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js"
+powershell -Command "(gc C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js) -replace 'var Debug=true;', 'var Debug=false;' | Out-File -Encoding UTF8  C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js"
 
-powershell -Command "(gc c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html) -replace 'type: \"GET\",', 'type: \"POST\",' | Out-File -Encoding ASCII  c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html"
+powershell -Command "(gc c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html) -replace 'type: \"GET\",', 'type: \"POST\",' | Out-File -Encoding UTF8  c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html"
 
 setup.py sdist
--- StartWebService.bat
+++ StartWebService.bat
@@ -1,6 +1,6 @@
-powershell -Command "(gc C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js) -replace 'var Debug=false;', 'var Debug=true;' | Out-File -Encoding ASCII   C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js"
+powershell -Command "(gc C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js) -replace 'var Debug=false;', 'var Debug=true;' | Out-File -Encoding UTF8   C:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\static\js\AppConfig.js"
 
-powershell -Command "(gc c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html) -replace 'type: \"POST\",', 'type: \"GET\",' | Out-File -Encoding ASCII  c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html"
+powershell -Command "(gc c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html) -replace 'type: \"POST\",', 'type: \"GET\",' | Out-File -Encoding UTF8  c:\Python\flaskweb\machinemonitoring-flask\chicken_attack\ui\templates\login.html"
 
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:5000/
 python chicken_attack\app.py
