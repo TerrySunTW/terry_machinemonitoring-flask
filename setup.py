f089facb056c881f7dca6f1ebac73b591e4e34c3
Terry <terry@example.com>
2017-11-05 01:09:49 +0800

    change version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chicken-attack',
-    version='0.6.19',
+    version='0.7.0',
     description='For terry test front end',
     author='WeiHao Huang',
     author_email='neilhuang@iii.org.tw',
