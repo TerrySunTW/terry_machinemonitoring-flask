97c660b1371decdc26a77e56a0916ea79500147e
Terry <terry@example.com>
2017-11-06 22:28:35 +0800

    Change datasoure to equipments api

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chicken-attack',
-    version='0.7.1',
+    version='0.7.2',
     description='For terry test front end',
     author='WeiHao Huang',
     author_email='neilhuang@iii.org.tw',
