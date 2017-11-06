e178ecf8bbff0a2daf660518e6c789555509484b
Terry <terry@example.com>
2017-11-06 12:56:41 +0800

    adjust UI & set cookie timeout in 1hr

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chicken-attack',
-    version='0.7.0',
+    version='0.7.1',
     description='For terry test front end',
     author='WeiHao Huang',
     author_email='neilhuang@iii.org.tw',
