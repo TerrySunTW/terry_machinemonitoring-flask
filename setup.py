934b588684aceff4f2e00f318d4a7c952623d0a4
Terry <terry@example.com>
2017-11-09 17:26:08 +0800

    adjust UI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chicken-attack',
-    version='0.7.4',
+    version='0.7.5',
     description='For terry test front end',
     author='WeiHao Huang',
     author_email='neilhuang@iii.org.tw',
