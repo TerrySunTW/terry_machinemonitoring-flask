8a5b8686a8bd6bcd5e8f7687969cd66f5f7d2ae3
Terry <terry@example.com>
2017-11-20 21:49:16 +0800

    fix detailview light status

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chicken-attack',
-    version='0.7.18',
+    version='0.7.19',
     description='For terry test front end',
     author='WeiHao Huang',
     author_email='neilhuang@iii.org.tw',
