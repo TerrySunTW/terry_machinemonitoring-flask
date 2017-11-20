f9dc1112e826a85c1a8ee129decbffd9570c7da8
Terry <terry@example.com>
2017-11-20 11:47:40 +0800

    fix step7 edit equipment bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chicken-attack',
-    version='0.7.17',
+    version='0.7.18',
     description='For terry test front end',
     author='WeiHao Huang',
     author_email='neilhuang@iii.org.tw',
