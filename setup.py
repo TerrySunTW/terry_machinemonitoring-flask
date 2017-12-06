774335d97692713715000691f02df29a9d66509b
Terry <terry@example.com>
2017-12-06 23:53:29 +0800

    add Modbus template selection function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chicken-attack',
-    version='0.7.25',
+    version='0.7.26',
     description='For terry test front end',
     author='WeiHao Huang',
     author_email='neilhuang@iii.org.tw',
